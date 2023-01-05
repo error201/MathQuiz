91f1726c8a246c9375ffe5885eab469f09e5b06c
Jason A. Arter <jason.arter@gmail.com>
2023-01-04 21:14:25 -0800

    Swapped javascript to JQuery.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,79 @@
 // Globals.
-var timeEl = document.querySelector("#time");
-var btnStart = document.querySelector("#start");
-var questionEl = document.querySelector("#question")
-var answerEl = document.querySelector("#question")
-var btnOne = document.querySelector("#btn1");
-var btnTwo = document.querySelector("#btn2");
-var btnThree = document.querySelector("#btn3");
+var timeEl = $("#time");
+var btnStart = $("#start");
+var questionEl = $("#question");
+var intIndex = 0;
 var isRunning = false;
 
 
-var questions = ["What is the only even prime number?", "When you divide a chart into four sections, what are those sections called?",
-                "This calculus operation uses an infinite sum to find the area under a curve.","What is the mnemonic to aid in remembering the trigonometric functions?","The Cartesian coordinate system is named after which French mathematician?"]
-var answers = [["0", "2", "4"],["sections" ,"quarters", "quadrants"],["integral", "limit", "addend"], ["Sohcahtoa", "Sesquahanna", "Sakajawea"],["Descartes" ,"Voltaire","Foucault"]]
+var questions = ["What is the only even prime number?",
+    "When you divide a chart into four sections, what are those sections called?",
+    "This calculus operation uses an infinite sum to find the area under a curve.",
+    "What is the mnemonic to aid in remembering the trigonometric functions?",
+    "The Cartesian coordinate system is named after which French mathematician?",
+    "Using the standard order of operations, what is the last operation you perform on an equation?",
+    "What is the measure of a right angle in degrees?",
+    "10 followed by 100 zeros is called what?",
+    "Which field of math is named for a mispronunciation of an Arabic phrase meaning 'reunion of broken parts'?",
+    "What broad category can be subdivided into studies of groups, rings, and fields?"]
+
+var answers = [["0", "2", "4"],
+["sections", "quarters", "quadrants"],
+["integral", "limit", "addend"],
+["Sohcahtoa", "Sesquahanna", "Sakajawea"],
+["Descartes", "Voltaire", "Foucault"],
+["exponents,", "multiplication", "subtraction"],
+["30'", "45'", "90'"],
+["googol", "zillion", "septazillion"],
+["x", "Algebra", ""],
+["", "", "Algebra"]]
+
+var keys = [1, 2, 0, 0, 0, 2, 2, 0, 1, 2]
 
 
-// TODO: start the game
+function init() {
+    // timeEl.hide();
+    // questionEl.hide();
+}
+
+
+// Start the game
 function start() {
     // Start timer.
-    if (isRunning){
+    if (isRunning) {
         return;
+    } else {
+        btnStart.text("STOP");
+        timeEl.show();
+        isRunning = true;
+        displayQuestion();
+        timeLeft = 30;
+        timeEl.text(timeLeft);
+        countdownTimer = setInterval(function () {
+            timeLeft--;
+            timeEl.text(timeLeft);
+            if (timeLeft <= 0) {
+                isRunning = false;
+                clearInterval(countdownTimer);
+                btnStart.text('START');
+                timeLeft = 60
+                timeEl.text(timeLeft);
+            }
+        }, 1000)
     }
-    isRunning = true;
-    displayQuestion();
-    timeLeft = 60;
-    timeEl.textcontext = timeLeft;
-    countdownTimer = setInterval(function(){
-        timeLeft--;
-        timeEl.textContent = timeLeft;
-        if(timeLeft <= 0) {
-            clearInterval(countdownTimer);
-            isRunning = false;
-        }
-    } ,1000)
 }
 
 
 // TODO: display question.
-function displayQuestion(){
-    myIndex = Math.ceil(Math.random()*questions.length);
-    questionEl.textContent = questions[myIndex];
-    btnOne.textContent = answers[myIndex][0];
-    btnTwo.textContent = answers[myIndex][1];
-    btnThree.textContent = answers[myIndex][2];
+function displayQuestion() {
+    intIndex = Math.floor(Math.random() * questions.length);
+    questionEl.text(questions[intIndex]);
 }
 
 
 // TODO: monitor for start button click.
-btnStart.addEventListener("click", start);
+btnStart.on("click", start);
 
 
 // TODO: check for correct answer.
-
+init();
