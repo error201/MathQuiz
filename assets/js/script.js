158feae117751b87ce3b3e8e9dd4003d7381e609
Jason A. Arter <jason.arter@gmail.com>
2023-01-05 16:59:27 -0800

    Completed basic functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,17 @@
 var timeEl = $("#time");
 var btnStart = $("#start");
 var questionEl = $("#question");
+var answerEl = $("#answers");
+
 var intIndex = 0;
 var isRunning = false;
 
+var iCorrect = 0;
+var iIncorrect = 0;
 
-var questions = ["What is the only even prime number?",
+// Questions.
+var questions = [
+    "What is the only even prime number?",
     "When you divide a chart into four sections, what are those sections called?",
     "This calculus operation uses an infinite sum to find the area under a curve.",
     "What is the mnemonic to aid in remembering the trigonometric functions?",
@@ -15,59 +21,79 @@ var questions = ["What is the only even prime number?",
     "What is the measure of a right angle in degrees?",
     "10 followed by 100 zeros is called what?",
     "Which field of math is named for a mispronunciation of an Arabic phrase meaning 'reunion of broken parts'?",
-    "What broad category can be subdivided into studies of groups, rings, and fields?"]
+    "What broad category can be subdivided into studies of groups, rings, and fields?"
+];
 
-var answers = [["0", "2", "4"],
-["sections", "quarters", "quadrants"],
-["integral", "limit", "addend"],
-["Sohcahtoa", "Sesquahanna", "Sakajawea"],
-["Descartes", "Voltaire", "Foucault"],
-["exponents,", "multiplication", "subtraction"],
-["30'", "45'", "90'"],
-["googol", "zillion", "septazillion"],
-["x", "Algebra", ""],
-["", "", "Algebra"]]
+// Answer options.
+var answers = [
+    ["0", "2", "4"],
+    ["sections", "quarters", "quadrants"],
+    ["integral", "limit", "addend"],
+    ["Sohcahtoa", "Sesquahanna", "Sakajawea"],
+    ["Descartes", "Voltaire", "Foucault"],
+    ["exponents", "multiplication", "subtraction"],
+    ["30°", "45°", "90°"],
+    ["googol", "zillion", "septazillion"],
+    ["Calculus", "Algebra", "Miltivariate"],
+    ["String Theory", "Set Theory", "Algebra"]
+];
 
+// Answer key (index in 'answers' array of the correct answer).
 var keys = [1, 2, 0, 0, 0, 2, 2, 0, 1, 2]
 
 
-function init() {
-    // timeEl.hide();
-    // questionEl.hide();
-}
-
-
-// Start the game
+// Start the game!
 function start() {
     // Start timer.
     if (isRunning) {
         return;
     } else {
-        btnStart.text("STOP");
         timeEl.show();
         isRunning = true;
         displayQuestion();
         timeLeft = 30;
-        timeEl.text(timeLeft);
+        timeEl.text("Time: " + timeLeft + "s");
         countdownTimer = setInterval(function () {
             timeLeft--;
-            timeEl.text(timeLeft);
+            timeEl.text("Time: " + timeLeft + "s");
             if (timeLeft <= 0) {
                 isRunning = false;
                 clearInterval(countdownTimer);
                 btnStart.text('START');
-                timeLeft = 60
-                timeEl.text(timeLeft);
+                timeLeft = 30
+                timeEl.text("Time: " + timeLeft + "s");
+                questionEl.text("");
+                answerLiEl = $("li");
+                if (answerLiEl) {
+                    $.each(answerLiEl, function (i, answerLiEl) {
+                        answerLiEl.remove();
+                    })
+                }
             }
         }, 1000)
     }
 }
 
 
-// TODO: display question.
+// Display a single question, and three answer option buttons.
 function displayQuestion() {
+    answerLiEl = $("li");
+    if (answerLiEl) {
+        $.each(answerLiEl, function (i, answerLiEl) {
+            answerLiEl.remove();
+        })
+    }
     intIndex = Math.floor(Math.random() * questions.length);
     questionEl.text(questions[intIndex]);
+    for (var i = 0; i < answers[intIndex].length; i++) {
+        var newLiEl = $("<li>")
+        var btnAnswer = $("<button>");
+        btnAnswer.addClass("answer-button");
+        btnAnswer.attr("data-key", i);
+        btnAnswer.text(answers[intIndex][i]);
+        newLiEl.append(btnAnswer);
+        answerEl.append(newLiEl);
+    }
 }
 
 
@@ -75,5 +101,16 @@ function displayQuestion() {
 btnStart.on("click", start);
 
 
-// TODO: check for correct answer.
-init();
+// TODO: check for correct answer with a delegated event listener.
+answerEl.on('click', '.answer-button', function (event) {
+    // Get the index to the answer from the button's data-key attribute.
+    var btnIndex = $(event.target).attr('data-key')
+    console.log(btnIndex);
+    if (keys[intIndex] == btnIndex) {
+        iCorrect += 1;
+        displayQuestion();
+    } else {
+        iIncorrect += 1;
+        displayQuestion();
+    }
+});
